c0e9e9d5abd8b5db15e2e373a47bacb67919609c
mJaJksJ <maksim.m00@mail.ru>
2022-09-14 14:18:22 +0300

    feat: настройка класса program
    добавлено логирование, настройка сваггера, константные директории

---
+++ MessengerCoreAPI/DirectoryPaths.cs
@@ -0,0 +1,26 @@
+using System.Reflection;
+
+namespace MessengerCoreAPI
+{
+    /// <summary>
+    /// Пути к директориям
+    /// </summary>
+    public static class DirectoryPaths
+    {
+        /// <summary>
+        /// Директория с запускаемым файлом
+        /// </summary>
+        public static string WorkingDirectory { get; private set; }
+
+        /// <summary>
+        /// Директория сохранения логов
+        /// </summary>
+        public static string LogsDirectory { get; private set; }
+
+        static DirectoryPaths()
+        {
+            WorkingDirectory = Assembly.GetExecutingAssembly().Location;
+            LogsDirectory = Path.Combine(WorkingDirectory, "Logs");
+        }
+    }
+}
--- MessengerCoreAPI/Program.cs
+++ MessengerCoreAPI/Program.cs
@@ -1,26 +1,93 @@
+using MessengerCoreAPI;
 using MessengerCoreAPI.Models.RGDialogsClients;
 using MessengerCoreAPI.Models.RGDialogsClients.RGDialogsClientsFactory;
+using Microsoft.OpenApi.Models;
+using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+var log = Log.ForContext<Program>();
+
+#region serilog configuration
+
+var logTemplateConsole = "[{Level:u3}] <{ThreadId}> :: {Message:lj}{NewLine}{Exception}";
+var logTemplateFile = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] <{ThreadId}> :: {Message:lj}{NewLine}{Exception}";
+
+if (!Directory.Exists(DirectoryPaths.LogsDirectory))
+{
+    try
+    {
+        Directory.CreateDirectory(DirectoryPaths.LogsDirectory);
+        log.Information($"Create directory {DirectoryPaths.LogsDirectory} for logs");
+    }
+    catch
+    {
+        log.Error($"Can't find or create directory {DirectoryPaths.LogsDirectory} for logs");
+        return;
+    }
+}
+
+builder.Host.UseSerilog((context, services, configuration) => configuration
+    .ReadFrom.Configuration(context.Configuration)
+    .ReadFrom.Services(services)
+    .Enrich.FromLogContext()
+    .Enrich.WithThreadId()
+    .WriteTo.Console(outputTemplate: logTemplateConsole)
+    .WriteTo.File(
+        outputTemplate: logTemplateFile,
+        path: Path.Combine(DirectoryPaths.LogsDirectory, "MessengerCoreApi.log"),
+        shared: true,
+        rollingInterval: RollingInterval.Day,
+        fileSizeLimitBytes: 128 * 1024 * 1024
+    )
+);
+
+#endregion serilog configuration
 
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+#region swagger settings
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "MessengerCoreAPI",
+        Version = "v1"
+    });
+
+    var xmlName = $"{Path.GetFileNameWithoutExtension(DirectoryPaths.WorkingDirectory)}.xml";
+    var xmlPath = Path.Combine(Path.GetDirectoryName(DirectoryPaths.WorkingDirectory), xmlName);
+
+    c.IncludeXmlComments(xmlPath);
+});
+
+#endregion swagger settings
+
+#region services
 
 IRGDialogsClientsFactory dialogsClientsFactory = new DefaultRGDialogsClientsFactory();
 var dialogs = new RGDialogsClientsCollection { RGDialogsClients = dialogsClientsFactory.CreateRGDialogsClients() };
 builder.Services.AddSingleton(dialogs);
 
+#endregion services
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseHsts();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "MessengerCoreAPI"); });
 
 app.UseHttpsRedirection();
 app.UseRouting();
